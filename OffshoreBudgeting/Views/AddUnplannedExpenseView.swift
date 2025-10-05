eacfa6cb9de70e085c8edda5fd1cd134b940e7cd
biiigshmike <brownmlee@icloud.com>
2025-10-05 10:04:51 -0700

    Revert "Align category chip rows with add button spacing"

--- OffshoreBudgeting/Views/AddUnplannedExpenseView.swift
+++ OffshoreBudgeting/Views/AddUnplannedExpenseView.swift
@@ -235,7 +235,6 @@ private struct CategoryChipsRow: View {
 
     // MARK: Local State
     @State private var isPresentingNewCategory = false
-    @State private var addButtonWidth: CGFloat = 0
     @Environment(\.platformCapabilities) private var capabilities
 
     private let verticalInset: CGFloat = DS.Spacing.s + DS.Spacing.xs
@@ -307,16 +306,13 @@ private extension CategoryChipsRow {
     }
 
     private func chipRowLayout() -> some View {
-        ZStack(alignment: .leading) {
-            chipsScrollView()
-                .padding(.leading, addButtonWidth + DS.Spacing.s)
+        HStack(alignment: .center, spacing: DS.Spacing.s) {
             addCategoryButton
+                .zIndex(1)
+            chipsScrollView()
         }
         .padding(.horizontal, DS.Spacing.s)
         .frame(maxWidth: .infinity, alignment: .leading)
-        .onPreferenceChange(AddCategoryButtonWidthPreferenceKey.self) { width in
-            addButtonWidth = width
-        }
     }
 
     private func chipsScrollView() -> some View {
@@ -356,20 +352,6 @@ private extension CategoryChipsRow {
         AddCategoryPill {
             isPresentingNewCategory = true
         }
-        .background(
-            GeometryReader { proxy in
-                Color.clear
-                    .preference(key: AddCategoryButtonWidthPreferenceKey.self, value: proxy.size.width)
-            }
-        )
-    }
-}
-
-private enum AddCategoryButtonWidthPreferenceKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
-
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
     }
 }
 

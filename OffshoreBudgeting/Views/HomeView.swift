27690ed957050ad62e04f38914d0e3fadabff037
biiigshmike <brownmlee@icloud.com>
2025-10-07 16:09:49 -0700

    Adjust Home toolbar glass transitions

--- OffshoreBudgeting/Views/HomeView.swift
+++ OffshoreBudgeting/Views/HomeView.swift
@@ -88,6 +88,7 @@ struct HomeView: View {
                                 .transition(.opacity.combined(with: .scale(scale: 0.95)))
                         }
                     }
+                    .animation(nil, value: hasActiveBudget)
                     .animation(nil, value: actionableSummaryForSelectedPeriod?.id)
                 } else {
                     // Legacy / older OS
@@ -214,6 +215,18 @@ struct HomeView: View {
         }
     }
 
+    private var addExpenseGlassTransition: Any? {
+        guard capabilities.supportsOS26Translucency else {
+            return nil
+        }
+
+        if #available(iOS 26.0, macOS 26.0, macCatalyst 26.0, *) {
+            return GlassEffectTransition.identity
+        } else {
+            return nil
+        }
+    }
+
     private var periodAdjustmentAnimation: Animation {
         .spring(response: 0.34, dampingFraction: 0.78, blendDuration: 0.1)
     }
@@ -283,7 +296,7 @@ struct HomeView: View {
                 glassNamespace: toolbarGlassNamespace,
                 glassID: HomeToolbarGlassIdentifiers.addExpense,
                 glassUnionID: capabilities.supportsOS26Translucency ? HomeGlassUnionID.main.rawValue : nil,
-                transition: toolbarGlassTransition
+                transition: addExpenseGlassTransition
             )
         }
         .modifier(HideMenuIndicatorIfPossible())
@@ -304,7 +317,7 @@ struct HomeView: View {
                 glassNamespace: toolbarGlassNamespace,
                 glassID: HomeToolbarGlassIdentifiers.addExpense,
                 glassUnionID: capabilities.supportsOS26Translucency ? HomeGlassUnionID.main.rawValue : nil,
-                transition: toolbarGlassTransition
+                transition: addExpenseGlassTransition
             )
         }
         .modifier(HideMenuIndicatorIfPossible())

ad6ee666b50e163ddd8c3121466deeb74c0dda8b
Michael <brownmlee@icloud.com>
2026-01-16 16:13:46 -0800

    2.1.1 (1) log message

--- OffshoreBudgeting/AppUpdateLogs/2.1.1.swift
+++ OffshoreBudgeting/AppUpdateLogs/2.1.1.swift
@@ -16,7 +16,12 @@ enum AppUpdateLog_2_1_1 {
                     TipsItem(
                         symbolName: "dollarsign.bank.building",
                         title: "Behind the Vault Improvements",
-                        detail: "Worked on restructuring the app, improving loading speeds, and helping Offshore prepare for the future of budgeting."
+                        detail: "Worked on restructuring the app to prepare Offshore for the future of budgeting. Swept a few dead bugs off the vault floor to help make the app run faster."
+                    ),
+                    TipsItem(
+                        symbolName: "creditcard.fill",
+                        title: "Improvements to Importing Expenses",
+                        detail: "Import process now automatically parses long bank Transaction Description text into a cleaner expense name. If you enable the toggle to use the expense name next time, the app can also remember your preference, locally on your device. This will make future imports suggest the saved name next time it's encountered."
                     )
                 ],
                 actionTitle: "Got It"
